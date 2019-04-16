fa89ba5c82c97f1e2ab810f52178fcede1ddab16
Mike Green <mike.is.green@gmail.com>
2019-04-16 10:46:46 -0400

    Fix time parsing for CLI invocation, write basic executable

--- bin/fireqos-autoscale.js
+++ bin/fireqos-autoscale.js
@@ -1,14 +1,13 @@
 #!/usr/bin/env node
 
-const { createReadStream, appendFile } = require('fs');
-const path = require('path');
+const Autoscaler = require('../lib/autoscaler');
 const chalk = require('chalk');
 const argv = require('yargs')
   .config()
   .help()
   .option('f', {
     alias: 'frequency',
-    default: '30m',
+    default: '15m',
     describe: 'Speed test and update frequency',
     type: 'string',
   })
@@ -24,20 +23,47 @@ const argv = require('yargs')
     describe: 'FireQOS config file output path',
     type: 'string',
   })
-  .option('u', {
-    alias: 'up-scale',
+  .option('w', {
+    alias: 'window',
+    default: 4,
+    describe: 'Number of previous results to average in calculating speed limit',
+    type: 'number',
+  })
+  .option('upScale', {
     default: 0.95,
     describe: 'Percentage of available upload bandwidth to use',
     type: 'number',
   })
-  .option('d', {
-    alias: 'down-scale',
+  .option('downScale', {
     default: 0.85,
     describe: 'Percentage of available download bandwidth to use',
     type: 'number',
   })
-  .option('l', {
-    alias: 'history-log',
-    describe: 'Path to file used to store past speed test results',
-    type: 'string',
+  .option('minUp', {
+    default: 500,
+    describe: 'Minimum upload speed (kbit/s)',
+    type: 'number',
+  })
+  .option('minDown', {
+    default: 500,
+    describe: 'Minimum download speed (kbit/s)',
+    type: 'number',
   }).argv;
+
+const scaler = new Autoscaler({
+  debug: true,
+  upScale: argv.upScale,
+  downScale: argv.downScale,
+  minUp: argv.minUp,
+  minDown: argv.minDown,
+  template: argv.template,
+  output: argv.output,
+  frequency: argv.frequency,
+  averageWindow: argv.window,
+});
+
+process.on('SIGINT', () => {
+  scaler.stop();
+});
+
+scaler.start();
